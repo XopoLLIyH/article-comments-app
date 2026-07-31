d79f2da71d252342d9e5117c9c16e8bcd142e426
IVAN <ivanberzin9@gmail.com>
2026-07-31 20:16:32 +0300

    fix(frontend): make Vite builds sandbox compatible

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,12 @@
   <v-app>
     <v-app-bar color="primary">
       <v-app-bar-title>Article Manager</v-app-bar-title>
-      <v-btn :to="{ name: 'articles' }">Articles</v-btn>
-      <v-btn :to="{ name: 'analytics' }">Comment analytics</v-btn>
+      <v-btn :to="{ name: 'articles' }">
+        Articles
+      </v-btn>
+      <v-btn :to="{ name: 'analytics' }">
+        Comment analytics
+      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -13,4 +17,3 @@
     </v-main>
   </v-app>
 </template>
-
